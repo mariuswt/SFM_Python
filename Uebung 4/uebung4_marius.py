91b48e85d289c56e22be64cb0b9fc1133fbd851e
MariusOberer <maobwt00@hs-esslingen.de>
2022-11-01 21:40:40 +0100

    anlegen Ordnerstruktur

---
+++ Uebung 4/uebung4_marius.py
@@ -0,0 +1,24 @@
+# Übung 4
+# Aufgabe 1
+# Beantworten Sie die folgenden Fragen:
+# 1. Erläutern Sie den Unterschied zwischen den Vergleichsoperatoren:
+# »==« und »is« sowie
+# »!=« und »is not«.
+
+# 2. Sie wollen den Rest der Division 225 / 17 ermitteln. Wie lautet die
+# Berechnung in python?
+
+# 3. Erläutern Sie die Short-Circuit-Evaluation. Nennen Sie ein Beispiel!
+
+# 4. Welchen Datentyp und Wert haben a und b?
+# a = 1 + 2 * 3 ** 4
+# b = 100 < a < 200
+
+# Aufgabe 2
+# Schreiben Sie ein Programm Zinsen, das einen aktuellen
+# Anfangskapitalwert K0 [Euro] mit einem Zinssatz p [%] nach n Jahren
+# verzinst:
+# Kn= K0 *(1 +p/100)^n
+# Testbeispiele:
+# K0 = 1000.00, n = 5, p = 2.0 folgt K5 = 1104.08
+# K0 = 1000.00, n = 5, p = −2.0 folgt K5 = 903.92
