5ca43fc57c2ec020d3f24fb5048dd00dc51e5cc9
MariusOberer <maobwt00@hs-esslingen.de>
2022-11-19 13:25:00 +0100

    create uebung6t

---
+++ Uebung 6/uebung6_marius.py
@@ -0,0 +1,54 @@
+# Übung 6
+
+# Aufgabe 1
+# 1. Definieren Sie in Python eine Liste mit den Vielfachen von 7, die kleiner als
+# 100 sind (also [7, 14,..., 98]).
+
+
+# 2. Welchen Datentyp verwenden Sie, um Lottozahlen zu speichern?
+
+
+# 3. Schreiben Sie ein Python-Script, das die folgende Matrix umsetzt:
+#
+# 2 4 6
+# 5 3 1
+#
+# und danach die Anzahl der Zeilen und die Anzahl der Spalten ausgibt.
+
+
+# 4. Erläutern Sie den Unterschied zwischen den folgenden Python-Befehlen für
+# zwei Sets x und y:
+# print (x – y)
+# print (y – x)
+
+
+# 5. Welches sind die gemeinsamen Buchstaben der Wörter „Python“ und
+# „Programmierung“?
+
+
+# 6. Entfernen Sie die Doppelgänger aus der folgenden Liste von Zahlen:
+# [1, 2, 3, 2, 7, 3, 9]. Die Ergebnisliste soll aufsteigend sortiert sein.
+
+
+# Aufgabe 2
+# a) Schreiben Sie ein Python-Script, das ein Dictionary erstellt mit den
+# Zahlen 1-100 als Schlüssel und einem Boolean-Wert, der angibt, ob die
+# Zahl eine Primzahl ist.
+
+# b) Als nächstes sollen die Zahlen 1-100 in zwei Listen sortiert werden:
+# Primzahlen sollen in eine Liste, Nicht-Primzahlen in eine andere Liste
+# einsortiert werden. Quelle der zu sortierenden Zahlen ist das Dictionary
+# aus a). Da geplant ist, später jederzeit neue Zahlen in das Dictionary
+# einzutragen, sollen die bereits sortierten Zahlen aus dem Dictionary
+# schrittweise entfernt werden.
+
+
+# Aufgabe 3
+# Schreiben Sie ein Python-Script, das eine Einkaufsliste verwaltet. Darin
+# sollen die Produkte und die Anzahl als Wert eingetragen werden:
+# {'Tomaten': 1, 'Gurken': 5, …}.
+# Zu Beginn ist die Einkaufsliste leer. Der Benutzer soll nun über die Eingabe
+# von „+Produkt“ ein Produkt hinzufügen, bzw. die Anzahl um 1 erhöhen, und
+# mit „-Produkt“ die Anzahl um 1 verkleinern, bzw. das Produkt löschen
+# können. Nach jeder Eingabe soll die Einkaufsliste auf dem Bildschirm
+# ausgegeben werden.
