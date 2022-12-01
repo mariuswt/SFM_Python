549f20c38206b53129959218246dfdc5af646992
MariusOberer <maobwt00@hs-esslingen.de>
2022-12-01 17:16:10 +0100

    Update uebung8_marius.py

--- Uebung 8/uebung8_marius.py
+++ Uebung 8/uebung8_marius.py
@@ -15,7 +15,6 @@ liste_mod7 = list(filter(ismodolo, liste))
 
 print(liste_mod7)
 
-
 # 2. Extrahieren Sie aus der Zeichenkette „Hello, World!“ alle Vokale, verbinden
 # Sie diese zu einer neuen Zeichenkette und geben Sie diese aus.
 
@@ -25,6 +24,7 @@ my_string = "Hello, World!"
 my_string_vocal = "".join(filter(is_vocal, my_string))
 print(my_string_vocal)
 
+
 # 3. Schreiben Sie eine Funktion vertauschen(), die als Übergabeparameter eine
 # Liste a und die zwei Indizes i und j für die zu vertauschenden Positionen
 # erhält. Nach dem Vertauschen sollen die Einträge in der Liste an den Stellen
@@ -33,3 +33,14 @@ print(my_string_vocal)
 # vertauschen (liste, 1, 3)
 # print (liste) # [Mi, Di, Mo, Do, Fr, Sa, So]
 # Ist dazu ein Rückgabewert notwendig? Testen Sie Ihre Funktion.
+def vertauschen(liste, i, j):
+    buffer = liste[i - 1]
+    liste[i - 1] = liste[j - 1]
+    liste[j - 1] = buffer
+    return liste
+
+
+liste = ["Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"]
+vertauschen(liste, 1, 3)
+print(liste)
+# Nein ein rückgabewert ist nicht notwendig! eine Liste ist Mutable, die Liste wird direkt am speicherort verändert
